210b4e56908c3244e8728b9e3f34fefa6ff66a52
rishavs <rishav.sharan@live.com>
2016-07-15 14:17:05 +0530

    Cleaned up some. Still trying to put code in separate functions

--- experiments/ConvexHull/main.lua
+++ experiments/ConvexHull/main.lua
@@ -5,8 +5,9 @@ local inspect = require "inspect"
 local vpolies_Count = 0
 local vpolies_Obj = {}
 local window_intercept
-local pbisector_line, connecting_line
+local pbisector_line
 local last_parent_site, last_child_site, last_bisector_point
+local neighbour_lines_obj
 local window_slope = 0
 local id_list = {}
 
@@ -52,25 +53,56 @@ function love.draw(dt)
     end
     
     love.graphics.setColor(255, 255, 255)
-    love.graphics.print("FPS: "..tostring(love.timer.getFPS( )), 10, 10)
+    love.graphics.print("FPS: "..tostring(love.timer.getFPS( )), 10, 10)    
+    love.graphics.print("Press [Space] to Reset", 100, 10)
     love.graphics.print(mousex .. ", " .. mousey, mousex -30, mousey -30)
 end
 
 function love.update(dt)
     mousex, mousey = love.mouse.getPosition()
 end
+
+
 function love.mousepressed(x, y, button, istouch)
     if button == 1 then
         -- add_to_points_Obj(x, y)
         add_to_vpolies_Obj(x, y)
-        print("ID list is: " , inspect(id_list))
+        print("\nID list is: " , inspect(id_list))
         print ("Poly List are : ")
         for id, vpoly in pairs(vpolies_Obj) do
-           print(id)
+           print("Poly " .. id .. " has neighbours " .. inspect(vpoly.neighbours))
        end
+    end
+    
+    if button == 2 then
+         for id, vpoly in pairs(vpolies_Obj) do
+            if vpoly.poly_Obj:testPoint( 0, 0, 0, x, y ) then
+                print("\n++++++++++++++++++++++++")
+                print("Right clicked on Ploygon ", id)
+                print ("\nIts Site is ..." , vpoly.site.x, vpoly.site.y)
+                print ("\nIts neighbours are ...")
+                print (inspect(vpoly.neighbours))
+                print("++++++++++++++++++++++++")
+             end
+         end
     end
 end
 
+function love.keypressed(key)
+    if key == "space" then
+        -- reset everything
+        vpolies_Count = 0
+        vpolies_Obj = {}
+        id_list = {}
+        pbisector_line = nil
+        connecting_line = nil
+        last_parent_site = nil
+        last_child_site = nil
+        last_bisector_point = nil
+        window_intercept = nil
+
+    end
+end
 --------------------------------------------------------------------
 
 function add_to_vpolies_Obj(sitex, sitey)
@@ -90,7 +122,7 @@ function add_to_vpolies_Obj(sitex, sitey)
                 love.graphics.getWidth(), love.graphics.getHeight(), 
                 0, love.graphics.getHeight() 
             ),
-            neighbour_indices = {}
+            neighbours = {}
         }
         -- local tmp_id = 
     vpolies_Obj[get_unique_random_str()] = temp_poly
@@ -102,17 +134,18 @@ function add_to_vpolies_Obj(sitex, sitey)
             if vpoly.poly_Obj:testPoint( 0, 0, 0, sitex, sitey ) then
                 print("\n--------------------------------------")
                 print("Point " .. sitex .. ", " .. sitey .. " is INSIDE the polygon with Polygon Id :", id)
+                print("It has neighbours : " .. inspect(vpoly.neighbours))
                 print("Getting Perpendicular Bisector between : " .. vpoly.site.x .. ", ".. vpoly.site.y .. " & " .. sitex .. ", " .. sitey)
                 xm, ym, bm, slope = get_perpendicular_bisector (vpoly.site.x, vpoly.site.y, sitex, sitey)
-                print(xm, ym, bm, slope)
-                print("Y = ".. slope .. "  X + " .. bm.. "\n")
+                print("Xm = " .. xm .. ", " .. "Ym = " .. ym .. ", " .. "Bm = " .. bm .. ", " .. "Slope = " .. slope)
+
                 window_intercept = get_window_intercept_of_line(ym, slope, xm, bm)
 
                 local poly_points = {vpoly.poly_Obj:getPoints()}
-                print ("Parent poly points are: " , inspect(poly_points))
+                print ("\nParent poly points are: " , inspect(poly_points))
                 local poly_sides = unpack_polygon (vpoly.poly_Obj:getPoints())
                 
-                connecting_line = {}
+                pbisector_line = {}
                 
                 -- for each side, find if intersect occurs
                 for _, side in pairs(poly_sides) do
@@ -126,8 +159,8 @@ function add_to_vpolies_Obj(sitex, sitey)
                     if clipping_point[1] and clipping_point[2] then
                         print("Clipping points are: [" .. clipping_point[1] .. ", " .. clipping_point[2] .. "]")
                         -- print(inspect(clipping_point))
-                        table.insert(connecting_line, clipping_point[1])
-                        table.insert(connecting_line, clipping_point[2])
+                        table.insert(pbisector_line, clipping_point[1])
+                        table.insert(pbisector_line, clipping_point[2])
                     end
                 end
                 
@@ -137,71 +170,118 @@ function add_to_vpolies_Obj(sitex, sitey)
                 local poly_points = {vpoly.poly_Obj:getPoints()}
                 
                 print("Splitting the Polygon with points...", inspect(poly_points))
-                local child_poly1_points = {}
-                local child_poly2_points = {}
+                local split_poly1_points = {}
+                local split_poly2_points = {}
                 
-                child_poly1_points, child_poly2_points = split_polygon  (poly_points, connecting_line[1], connecting_line[2], connecting_line[3], connecting_line[4])
+                split_poly1_points, split_poly2_points = split_polygon  (poly_points, pbisector_line[1], pbisector_line[2], pbisector_line[3], pbisector_line[4])
                 
-                print("POLY 1 is ", inspect(child_poly1_points))
-                print("POLY 2 is ", inspect(child_poly2_points))
-                    
-                temp_id1 = get_unique_random_str()
-                temp_id2 = get_unique_random_str()
+                print("POLY 1 vertices are: ", inspect(split_poly1_points))
+                print("POLY 2 vertices are: ", inspect(split_poly2_points))
                 
-                print("Temp ids are: ", temp_id1, temp_id2)
+                local parent_poly_id, child_poly_id
+                local parent_poly = {}
+                local child_poly = {}
+                
+                
+                parent_poly_id = id
+                child_poly_id = get_unique_random_str()
+            
+                parent_poly.site = vpoly.site
+                child_poly.site = {x = sitex, y = sitey}
+                
+                parent_poly.color = vpoly.color
+                child_poly.color = {get_random_color()}
+                
+                parent_poly.neighbours = vpoly.neighbours
+                child_poly.neighbours = {}
+                
+                -- parents and children are always neighbours of each other
+                table.insert(parent_poly.neighbours, child_poly_id)
+                table.insert(child_poly.neighbours, parent_poly_id)
                 
                 -- add polies to the poly object
-                temp_poly1 = { 
-                    index = vpolies_Count,
-                    site = {x=sitex, y=sitey},
-                    color =  {get_random_color()},
-                    poly_Obj =  love.physics.newPolygonShape ( child_poly1_points ),
-                    neighbour_indices = {}
-                }                
+                local temp_poly1 = love.physics.newPolygonShape ( split_poly1_points )
+                local temp_poly2 = love.physics.newPolygonShape  ( split_poly2_points )
                 
-                temp_poly2 = { 
-                    index = vpolies_Count,
-                    site = {x=sitex, y=sitey},
-                    color =  {get_random_color()},
-                    poly_Obj =  love.physics.newPolygonShape  ( child_poly2_points ),
-                    neighbour_indices = {}
-                }
-                -- now check in wich polygon does the parent site lies 
-                local temp1 = get_side_of_line(connecting_line[1], connecting_line[2], connecting_line[3], connecting_line[4], vpoly.site.x, vpoly.site.y)
-                if  temp1 == 1 then
-                    temp_poly1.site = vpoly.site
-                    temp_poly2.site = {x = sitex, y = sitey}
-                elseif temp1 == -1 then
-                    temp_poly1.site = {x = sitex, y = sitey}
-                    temp_poly2.site = vpoly.site
-                elseif temp1 == 0 then
-                    print("Parent site lies on bisector line??!! what nonsense!")
+                -- reuse and set new ids
+                if temp_poly1:testPoint( 0, 0, 0, vpoly.site.x, vpoly.site.y ) then
+                    parent_poly.poly_Obj = temp_poly1
+                    child_poly.poly_Obj = temp_poly2
+                    
+                elseif temp_poly2:testPoint( 0, 0, 0, vpoly.site.x, vpoly.site.y ) then
+                    parent_poly.poly_Obj = temp_poly2
+                    child_poly.poly_Obj = temp_poly1
                 end
                 
-                -- check if the new plies are valid and if yes, then add them to the main poly object
-                if  temp_poly1.poly_Obj:validate( ) and temp_poly1.poly_Obj:validate( ) then
-                -- reomve parent polygon
-                    print("Removing Parent poly : ", id)
-                    vpolies_Obj[id] = nil
-                    id_list[id] = nil
-                    
-                --insert into poly object
-                    vpolies_Obj[temp_id1] = temp_poly1
-                    vpolies_Obj[temp_id2] = temp_poly2
-                -- now for the line joiing the parent site witrh child site
-                    last_parent_site = {vpoly.site.x, vpoly.site.y}
-                    last_child_site = {sitex, sitey}
-                    last_bisector_point = {Xm, Ym}
-
-                -- merge with new poly
+                -- If both polies are valid then add them to the main poly object
+                if  parent_poly.poly_Obj:validate( ) and child_poly.poly_Obj:validate( ) then                    
+                    --insert into poly object
+                    vpolies_Obj[parent_poly_id] = parent_poly
+                    vpolies_Obj[child_poly_id] = child_poly
                 else
                     print ("SPLIT POLYGONS ARE NOT VALID!!!")
                 end
+
+                    
+
                 
+                
+
+                
+                local potential_parent_neighbours = vpoly.neighbours
+                local potential_child_neighbours = vpoly.neighbours
+                print(potential_child_neighbours)
+                -- iterate through potential neighbours for bot parent and child and discard all values which are not neighbours
+                if next(potential_child_neighbours) then
+                
+                    print("Potential Neighbours of child polygon are: ")
+                    for i, neighbour in ipairs(potential_child_neighbours) do
+                        
+                        print(neighbour)
+                        
+                        
+                        
+                    end
+                end
+                -- for i, neighbour in ipairs(potential_child_neighbours) do
+                
+                -- end
+                
+                -- then add the parent to child and child to parents neighbours list
+                -- now we check if ech potential neighbour is actualy a neighbour (has line intersection or shared side) or not
+                -- if yes, then add it to the neighbours list for current pol
+                
+                    
+
+                
+                
+                -- if find neighbour also update neighbours so that it gets current pol in neighbors list
+                -- repeat task with all neighbouring polies which share a side with parent poly
+                
+                
+                
+                -- we will also need to update the neighbors list for the parent poly. it might have lost some.
+                
+                print ( "Parent Polygon's Neighbours are: " .. inspect( vpolies_Obj[parent_poly_id].neighbours))
+                print ( "Child Polgygon's Neighbours are: " .. inspect( vpolies_Obj[child_poly_id].neighbours))
+                    
+                    
+                -- now for the line joiing the parent site with child site
+                last_parent_site = {vpoly.site.x, vpoly.site.y}
+                last_child_site = {sitex, sitey}
+                last_bisector_point = {Xm, Ym}
+                    
+                -- Add neighbours to neighbour_lines_obj so we can draw them
+                
+                
+                
+                -- check if the new plies are valid and if yes, then add them to the main poly object
+
 
                 -- find neighbours of the new poly and add to dataset
                 
-                -- repeat task with all neighbouring polies which share a side with parent poly
+                
+
                 
                 
                 break
@@ -327,50 +407,50 @@ function get_lineEqn_intersection (Xm, Ym, slope, bm, m, b, x1, y1, x2, y2)
         if m == 0 and (slope == math.huge or slope == -math.huge) then
             iy = y1
             ix = Xm
-            print(ix, iy)
-            print("Scenario :  m1 = 0 and m2 = inf")
+            -- print(ix, iy)
+            -- print("Scenario :  m1 = 0 and m2 = inf")
             
         -- m1 = inf and m2 = 0
         elseif (m == math.huge or m == -math.huge) and slope == 0 then
             iy = Ym
             ix = x1
-            print(ix, iy)
-            print("Scenario :  m1 = inf and m2 = 0")
+            -- print(ix, iy)
+            -- print("Scenario :  m1 = inf and m2 = 0")
             
         -- m1 = 0 and m2 = valid
         elseif m == 0 and (slope ~= math.huge or slope ~= -math.huge) then
             iy = y1
             ix = (iy - bm) / slope
-            print(ix, iy)
-            print("Scenario :  m1 = 0 and m2 = valid")
+            -- print(ix, iy)
+            -- print("Scenario :  m1 = 0 and m2 = valid")
             
         -- m1 = valid and m2 = 0 --
         elseif (m ~= math.huge or m ~= -math.huge) and slope == 0 then
             iy = Ym
             ix = (iy - b) / m
-            print(ix, iy)
-            print("Scenario :  m1 = valid and m2 = 0")
+            -- print(ix, iy)
+            -- print("Scenario :  m1 = valid and m2 = 0")
             
         -- m1 = inf and m2 = valid
         elseif (m == math.huge or m == -math.huge) and  (slope ~= math.huge or slope ~= -math.huge) then
             ix = x1
             iy = (slope * ix) + bm
-            print(ix, iy)
-            print("Scenario :  m1 = inf and m2 = valid")
+            -- print(ix, iy)
+            -- print("Scenario :  m1 = inf and m2 = valid")
         
         -- m1 = valid and m2 = inf
         elseif (m ~= math.huge or m ~= -math.huge) and  (slope == math.huge or slope == -math.huge) then
             ix = Xm
             iy = (m * ix) + b
-            print(ix, iy)
-            print("Scenario :  m1 = valid and m2 = inf")
+            -- print(ix, iy)
+            -- print("Scenario :  m1 = valid and m2 = inf")
             
         -- m1 = valid and m2 = valid
         else
             ix = -( (b - bm)/(m - slope))
             iy = (slope * ix) + bm
-            print(ix, iy)
-            print("Scenario :  m1 = valid and m2 = valid")
+            -- print(ix, iy)
+            -- print("Scenario :  m1 = valid and m2 = valid")
         end
         -- print("Testing point : " .. ix, iy)
         
